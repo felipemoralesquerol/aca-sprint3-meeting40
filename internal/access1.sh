603d057a06789f67f6abe989613eb44b83db429b
Felipe Morales <felipe.morales.querol@gmail.com>
2021-11-11 00:48:02 -0300

    first commit

---
+++ internal/access1.sh
@@ -0,0 +1,3 @@
+#chmod 400 deploy-dwbe.pem
+ssh -i "deploy-dwbe.pem" ec2-user@ec2-18-118-17-248.us-east-2.compute.amazonaws.com
+
---
+++ internal/access4.sh
@@ -0,0 +1,1 @@
+ssh -i "aca-acceso.pem" ec2-user@ec2-3-144-103-63.us-east-2.compute.amazonaws.com
